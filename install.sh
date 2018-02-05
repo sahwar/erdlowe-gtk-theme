3af5a1ad76f1a2fdb0ac5fac0ad428ea543aa425
DarthWound <darthwound@live.com>
2018-02-05 23:25:16 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 # Erdlöwe theme by DarthWound - install
 
 mkdir ~/.bin
-cd ~/.bin
-wget https://raw.githubusercontent.com/DarthWound/erdlowe-theme/master/erdlowe.sh
-chmod +x erdlowe.sh
+wget -P ~/.bin https://raw.githubusercontent.com/DarthWound/erdlowe-theme/master/erdlowe.sh
+chmod +x ~/.bin/erdlowe.sh
 printf "\n\n\e[1mScript installed\e[0m - run it with '~/.bin/erdlowe.sh' command.\n\n"
