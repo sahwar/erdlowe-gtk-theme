d9003dcdecf8be63ceaf787a67477786526b7d62
DarthWound <darthwound@live.com>
2018-02-05 23:20:07 +0100

    Create install.sh
    Making installation easier.

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Erdlöwe theme by DarthWound - install
+
+mkdir ~/.bin && cd ~/.bin
+wget https://raw.githubusercontent.com/DarthWound/erdlowe-theme/master/erdlowe.sh
+chmod +x erdlowe.sh
+printf "\n\n\e[1mScript installed\e[0m - run it with '~/.bin/erdlowe.sh' command.\n\n"
